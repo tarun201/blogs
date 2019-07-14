ddf9851263495a55f2b141dacb0a77a7a30a5244
Tarun <dragoon@dragoon>
2019-07-14 12:38:50 +0530

    change according to readme file

--- node/config/index.js
+++ node/config/index.js
@@ -5,6 +5,6 @@ config.port = '3306';
 config.db = 'internship';
 config.user = 'root';
 config.passwd = '1234';
-config.servicePort = 8080;
+config.servicePort = 8080; //Don't change this
 
 module.exports = config;
