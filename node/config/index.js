609665d9e421ab5c098a575f0d73223918f2b59c
Tarun Goenka <tarun.goenka12@gmail.com>
2019-07-14 12:58:18 +0530

    Update index.js

--- node/config/index.js
+++ node/config/index.js
@@ -2,9 +2,9 @@
 var config = {};
 config.host = 'localhost';
 config.port = '3306';
-config.db = 'internship';
-config.user = 'root';
-config.passwd = '1234';
+config.db = '****';
+config.user = '****';
+config.passwd = '****';
 config.servicePort = 8080; //Don't change this 
 
 module.exports = config;
